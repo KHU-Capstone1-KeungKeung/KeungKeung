cac1e81b31fce295955b553574629fa55a30bb15
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-05-10 20:26:56 +0900

    Update createSignalingChannel

--- App.js
+++ App.js
@@ -513,6 +513,26 @@ const App = () => {
     }
   }
 
+  const createSignalingChannel = async () => {
+    // Create KVS client
+    const kinesisVideoClient = new AWS.KinesisVideo({
+      region: Config.REGION,
+      accessKeyId: Config.ACCESS_KEY_ID,
+      secretAccessKey: Config.SECRET_ACCESS_KEY,
+      sessionToken: null,
+      endpoint: null,
+    });
+
+    // Get signaling channel ARN
+    const describeSignalingChannelResponse = await kinesisVideoClient
+        .describeSignalingChannel({
+          ChannelName: Config.CHANNEL_NAME,
+        })
+        .promise();
+    const channelARN = describeSignalingChannelResponse.ChannelInfo.ChannelARN;
+    console.log('[CREATE_SIGNALING_CHANNEL] Channel ARN: ', channelARN);
+  }
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
